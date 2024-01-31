fcd057ed0872c582b3a13ea667d7e8b72dd3ab64
sanja <sanja@192.168.1.10>
2024-01-31 20:27:41 +0530

    first commit

---
+++ Day01/src/com/amc/arrays/ArrayOfObjects.java
@@ -0,0 +1,20 @@
+package com.amc.arrays;
+
+public class ArrayOfObjects {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Student arr[]=new Student[5];
+		arr[0]=new Student(11,"Rahul");
+		arr[1]=new Student(12,"Ravi");
+		arr[2]=new Student(13,"Ravin");
+		arr[3]=new Student(14,"Rohith");
+		arr[4]=new Student(15,"Roshan");
+		
+		for(Student s:arr) {
+			System.out.println(s.getAge()+ " "+s.getName());
+		}
+
+	}
+
+}
